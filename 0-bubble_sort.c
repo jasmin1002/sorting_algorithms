e4f09e34dd61c0ba6ddebdf720c25b825f9bb0c7
Jasmine <jasmineolofin@gmail.com>
2022-07-16 10:26:06 +0000

    modify bubble_sort template file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,4 +10,28 @@
   */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+
+	for(i = 0; i < size - 1; i++)
+	{
+		int flag = 0;
+		size_t new_size = size - 1 - i;
+
+		for(j = 0; j < new_size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+
+				flag = 1;
+			}
+		}
+
+		if (flag == 0)
+			break;
+	}
+
+	print_array(array, size);
 }
