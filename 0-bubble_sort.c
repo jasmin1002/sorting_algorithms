b1f43cd7277baf262a16e57a749980acf9f37c30
Jasmine <jasmineolofin@gmail.com>
2022-07-16 08:52:54 +0000

    commit 0-bubble_sort template file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - Sorts an array of integers in asc order
+  *
+  * @array: Input array to sort
+  * @size: Number of elements in @array
+  *
+  * Return: nothing
+  */
+void bubble_sort(int *array, size_t size)
+{
+}
