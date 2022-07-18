de27f04e7f22bb72345fd4d120537765adbf6e4f
Jasmine <jasmineolofin@gmail.com>
2022-07-18 22:21:32 +0000

    Add to 1-insertion_sort_list template

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+  * insertion_sort_list - Sorts a doubly linked list of integer in asc
+  *
+  * @list: Input list to sort
+  *
+  * Return: Nothing
+  */
+void insertion_sort_list(listint_t **list)
+{
+}
+  
