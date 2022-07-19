8ddb606ea25f5978c6b65fcf546b87c2d329834a
Jasmine <jasmineolofin@gmail.com>
2022-07-19 18:24:33 +0000

    commit quick sort algorithm boiler plate

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+  * quick_sort - Sorts an array of integers in ascending order
+  *
+  * @array: Array of integers to sort
+  * @size: Size or length of the given array
+  *
+  * Return: void(nothing)
+  */
+void quick_sort(int *array, size_t size)
+{
+}
