4e94ef237e830e11a9d32dad3fc52b12e6add560
Jasmine <jasmineolofin@gmail.com>
2022-07-23 22:46:10 +0000

    Add shell_sort prototype

--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void selection_sort(int *, size_t);
 void quick_sort(int *, size_t);
 void QuickSort(int *, int, int, size_t);
 int partition(int *, int, int, size_t);
+void shell_sort(int *, size_t);
 
 
 #endif
